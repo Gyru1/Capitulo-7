3413c5d015fc349b8a9670d4bcbe40e7ae3475fe
Gyru1 <43257990+Gyru1@users.noreply.github.com>
2018-10-22 21:41:35 -0300

    Add files via upload

---
+++ ejemplo_7.m
@@ -0,0 +1,6 @@
+﻿x1 = [1 1 2];
+x2 = [1 2 3]; %Se obtienen las siguientes muestras de y:
+X1 = dft(x1);
+X2 = dft(x2); %8.0000
+Y = X1 .* X2; %9.0000 - 0.0000i
+y = idft(Y)'
---
+++ usar_dft.m
@@ -0,0 +1,4 @@
+﻿x = [1 1 0 0 0];
+X = dft(x);
+X.' % despliega en forma de columna
+% .' significa "transpuesta no-conjugada"
